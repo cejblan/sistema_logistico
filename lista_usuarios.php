66318736df51d304b2fd60e916fab3cf8b84f461
Francisco González <benfran21@hotmail.com>
2022-12-15 11:43:37 -0400

    Se termino la lista de usuarios

--- lista_usuarios.php
+++ lista_usuarios.php
@@ -30,3 +30,126 @@ if(!isset($_SESSION['usuario']))
             }
           }
 admin:
+
+$consult_usuarios = "SELECT * FROM usuarios";
+$result_usuarios = mysqli_query($conexion,$consult_usuarios);
+
+if(!$result_usuarios) 
+  {
+  echo "No se ha podido realizar la consulta";
+  }
+
+if (mysqli_num_rows($result_usuarios) == true)
+  {
+?>
+<section class="container FEs">
+  <div class="row">
+    <div class="col-12">
+        <h1>Lista de Usuarios</h1>
+    </div>
+  </div>
+  </br>
+  </br>
+  <div class="row">
+    <div class="col-12">
+      <table class="table table-dark">
+        <thead>
+          <tr>
+            <th>
+              <label>Cedula</label>
+            </th>
+            <th>
+              <label>Nombre</label>
+            </th>
+            <th>
+              <label>Apellido</label>
+            </th>
+            <th>
+              <label>Fecha</label>
+            </th>
+            <th>
+              <label>Creador</label>
+            </th>
+            <th>
+              <label>Acción</label>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+
+        </tbody><?php
+  while ($columP = mysqli_fetch_array($result_usuarios))
+    {
+    echo '<tr>
+            <td>
+              <label class="padding">
+                <h6>' . $columP['cedula'] . '</h6>
+              </label>
+            </td>
+            <td>
+              <label class="padding">
+                <h6>' . $columP['nombre'] . '</h6>
+              </label>
+            </td>
+            <td>
+              <label class="padding">
+                <h6>' . $columP['apellido'] . '</h6>
+              </label>
+            </td>
+            <td>
+              <label class="padding">
+                <h6>' . $columP['fecha'] . '</h6>
+              </label>
+            </td>
+            <td>
+              <label class="padding">
+                <h6>' . $columP['creador'] . '</h6>
+              </label>
+            </td>
+            <td>
+              <label>
+                <form action="delete.php" method="POST">
+                  <input type="text" name="eliminar" id="eliminar" class="form-control" value="'. $columP['cedula'] . '" readonly required/>
+                  <button id="submitEliminar" class="button-presupuesto btn btn-primery" type="submit">Eliminar</button>
+                </form>
+              </label>
+            </td>
+          </tr>';
+    }
+
+  echo '  </tbody>
+        </table>
+      </div>
+    <a href="administrativo.php">
+      <button id="eliminar" class="button-presupuesto btn btn-success" type="submit">volver</button>
+    </a>
+  </div>
+</section>';
+  }
+  else
+      {
+      echo '
+            </tbody>
+          </table>
+            <h4>
+              <a href="page2.php">
+                <button id="eliminar" class="button-presupuesto btn btn-primery" type="submit">Volver</button>
+              </a>Productos eliminados
+            </h4>
+          </br>';
+      goto end;
+  }   
+
+end:
+@mysqli_close( $conexion );
+
+?>
+
+      </div>
+    </div>
+  </div>
+</section>
+
+<?php
+require_once('php/footer.php');
+?>
